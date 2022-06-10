6a1c0c507adc94c07c2e50595d06e053b42f8325
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-06-10 20:23:37 +0200

    refactor(works): update style for buttonsand increase duration of animation

--- components/Works.js
+++ components/Works.js
@@ -3,33 +3,32 @@ import { Box } from "@chakra-ui/react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Button } from '@chakra-ui/react';
 import PreviewIcon from '@mui/icons-material/Preview';
-import { motion,  } from "framer-motion";
+import { motion, } from "framer-motion";
 
 export function FadeInWhenVisible({ children }) {
     return (
-      <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      transition={{ duration: 1}}
-      variants={{
-        visible: { y: 0, opacity: 1},
-        hidden: { y: 100, opacity: 0 }
-        }}
-      >
-        {children}
-      </motion.div>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 1 }}
+            variants={{
+                visible: { y: 0, opacity: 1 },
+                hidden: { y: 100, opacity: 0 }
+            }}
+        >
+            {children}
+        </motion.div>
     );
-  }
+}
 
 export default function Works() {
 
     return (
-        <section id="works" className="full-width bg-works">
-            <h1 className="text-center">Works</h1>
-            <br />
+        <section id="works" className="full-width works-bg">
+            <h1 className="text-center section-title">Last works</h1>
             <div className="flex-row">
-            <FadeInWhenVisible><Box className="box-works" maxW='sm' borderWidth='1px' borderRadius='lg' m={2} w={350} p={5} color='black' >
+                <FadeInWhenVisible><Box className="box" maxW='sm' borderRadius={15} m={2} w={350} p={5} color='black' >
                     <h3>Budget-Manger</h3>
                     <Image
                         borderRadius={5}
@@ -41,11 +40,11 @@ export default function Works() {
                     <aside>Technologies: JavaScript, Express.js, React, MongoDB, Charts.js, Material UI, HTML, CSS</aside>
                     <br />
                     <div className="flex-row">
-                        <a href="https://github.com/FreiburgDevTeam/mern-application-client" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;View source </Button></a>
-                        <a href="https://budget-manager-mern-app.netlify.app/" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;View demo </Button></a>
+                        <a href="https://github.com/FreiburgDevTeam/mern-application-client" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;Source </Button></a>
+                        <a href="https://budget-manager-mern-app.netlify.app/" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;Demo </Button></a>
                     </div>
                 </Box></FadeInWhenVisible>
-                <FadeInWhenVisible><Box className="box-works" maxW='sm' borderWidth='1px' borderRadius='lg' m={2} w={350} p={5} color='black' >
+                <FadeInWhenVisible><Box className="box" maxW='sm' borderRadius={15} m={2} w={350} p={5} color='black' >
                     <h3>PetSharing</h3>
                     <Image
                         borderRadius={5}
@@ -56,11 +55,11 @@ export default function Works() {
                     <aside>Technologies: JavaScript, Express.js, Handlebars, MongoDB, HTML, CSS </aside>
                     <br />
                     <div className="flex-row">
-                        <a href="https://github.com/FreiburgDevTeam/crud-project" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;View source </Button></a>
-                        <a href="https://pet-sharing.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;View demo </Button></a>
+                        <a href="https://github.com/FreiburgDevTeam/crud-project" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;Source </Button></a>
+                        <a href="https://pet-sharing.herokuapp.com/" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;Demo </Button></a>
                     </div>
                 </Box></FadeInWhenVisible>
-                <FadeInWhenVisible><Box className="box-works" maxW='sm' borderWidth='1px' borderRadius='lg' m={2} w={350} p={5} color='black' >
+                <FadeInWhenVisible><Box className="box" maxW='sm' borderRadius={15} m={2} w={350} p={5} color='black' >
                     <h3>The Good Diver</h3>
                     <Image
                         borderRadius={5}
@@ -68,16 +67,16 @@ export default function Works() {
                         src='../images/works/the-good-diver-js-game.png'
                         alt='The Good Diver' />
                     <p>The Good Diver is a game about cleaning the ocean.
-In order to earn points the you must capture ocean trash without being attacked by the sharks.
-You can move the diver by using the the arrow keys.</p>
+                        In order to earn points the you must capture ocean trash without being attacked by the sharks.
+                        You can move the diver by using the the arrow keys.</p>
                     <aside>Technologies: Vanilla JavaScript, HTML, CSS </aside>
                     <br />
                     <div className="flex-row">
-                        <a href="https://github.com/oscargonzalezdev/project-game" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;View source </Button></a>
-                        <a href="https://oscargonzalezdev.github.io/project-game" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;View demo </Button></a>
+                        <a href="https://github.com/oscargonzalezdev/project-game" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><GitHubIcon fontSize="inherit" />&nbsp;Source </Button></a>
+                        <a href="https://oscargonzalezdev.github.io/project-game" target="_blank" rel="noopener noreferrer"><Button color='black' variant='outline'><PreviewIcon fontSize="inherit" />&nbsp;Demo </Button></a>
                     </div>
                 </Box></FadeInWhenVisible>
             </div>
-            </section>
+        </section>
     )
 }
