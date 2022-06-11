e428e289a7a0bae15183be0e366d57b1f5aa2262
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-06-11 18:31:34 +0200

    fix(layout): update path to layout component and delete hello.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { ChakraProvider } from '@chakra-ui/react'
 
 function MyApp({ Component, pageProps }) {
