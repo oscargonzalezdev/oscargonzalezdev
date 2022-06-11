71f7ed6639bf8995b762fb8e2f983ca7cb39c9e4
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-06-11 14:43:21 +0200

    style(footer): add style to footer component

--- components/Footer.js
+++ components/Footer.js
@@ -6,17 +6,18 @@ import { Button } from '@chakra-ui/react';
 function Footer() {
     return (
         <section className="main-container">
-        <div className="footer-bg">
-        <p>Made with 🤍 and ☕ by Oscar Gonzalez</p>
-        <br />
-        <span className='social-links'>
-                  <a href="https://github.com/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><GitHubIcon /></a>
-                  <a href="https://www.linkedin.com/in/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><LinkedInIcon /></a>
-                  <a href="https://twitter.com/oscardev_" target="_blank" rel="noopener noreferrer" ><TwitterIcon /></a>
-                </span>
+            <div className="footer-bg">
+                <div className='flex-row flex-center'>
+                    <p>Made with 🤍 and <span className='emoji-coffee'>☕</span> by Oscar Gonzalez &nbsp;</p>
+                    <a href="https://github.com/oscargonzalezdev/portfolio" target="_blank" rel="noopener noreferrer"><Button borderColor='#c1c1c1' color='#c1c1c1' _hover={{ bg: '#fff', color: '#000' }} size='xs' variant='outline' ><GitHubIcon fontSize="inherit" />&nbsp; View Source </Button></a>
+                </div>
                 <br />
-                <a href="https://github.com/oscargonzalezdev/portfolio" target="_blank" rel="noopener noreferrer"><Button color='gray' size='xs' variant='outline' ><GitHubIcon fontSize="inherit" />&nbsp; View Source </Button></a>
-        </div>
+                <span className='social-links'>
+                    <a href="https://github.com/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><GitHubIcon /></a>
+                    <a href="https://www.linkedin.com/in/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><LinkedInIcon /></a>
+                    <a href="https://twitter.com/oscardev_" target="_blank" rel="noopener noreferrer" ><TwitterIcon /></a>
+                </span>
+            </div>
         </section>
     )
 }
