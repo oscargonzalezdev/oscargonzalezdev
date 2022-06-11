ba2a18535fa29f88a60caaf690453fbfafca1c5f
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-06-11 19:23:01 +0200

    fix(contact): remove inline style properties of Box component to avoid conflicts with globals.css

--- components/Contact.js
+++ components/Contact.js
@@ -56,47 +56,42 @@ export default function ContactForm() {
 
     return (
         <section id="contact" className='full-width contact-bg'>
-        <ToastContainer position="bottom-right" />
+            <ToastContainer position="bottom-right" />
             <div className='container flex-column'>
                 <form className='contact-form' ref={form} onSubmit={sendEmail}>
                     <h1 className='text-center section-title'>Get in touch</h1>
                     <br />
-                    <FadeInWhenVisible ><Box className='box'
-                        bg={useColorModeValue('white', 'gray.100')}
-                        borderRadius="lg"
-                        p={8}
-                        color={useColorModeValue('gray.700', 'whiteAlpha.900')}
-                        shadow="base">
-                        <VStack spacing={5}>
-                            <FormControl isRequired>
-                                <FormLabel>Name</FormLabel>
+                    <FadeInWhenVisible >
+                        <Box className='box'>
+                            <VStack spacing={5}>
+                                <FormControl isRequired>
+                                    <FormLabel>Name</FormLabel>
+                                    <InputGroup>
+                                        <Input type="text" name="user_name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                    </InputGroup>
+                                </FormControl>
 
-                                <InputGroup>
-                                    <Input type="text" name="user_name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
-                                </InputGroup>
-                            </FormControl>
+                                <FormControl isRequired>
+                                    <FormLabel>Email</FormLabel>
+                                    <InputGroup>
+                                        <Input type="email" name="user_email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                    </InputGroup>
+                                </FormControl>
 
-                            <FormControl isRequired>
-                                <FormLabel>Email</FormLabel>
-
-                                <InputGroup>
-                                    <Input type="email" name="user_email" placeholder="Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                                </InputGroup>
-                            </FormControl>
-
-                            <FormControl isRequired>
-                                <FormLabel>Message</FormLabel>
-                                <Textarea name="message" placeholder="Your Message" rows={9} resize="none" value={message} onChange={(e) => setMessage(e.target.value)} />
-                            </FormControl>
-                            <Button
-                                type="submit"
-                                color='black'
-                                variant='outline'
-                            >
-                                Send Message
-                            </Button>
-                        </VStack>
-                    </Box></FadeInWhenVisible>
+                                <FormControl isRequired>
+                                    <FormLabel>Message</FormLabel>
+                                    <Textarea name="message" placeholder="Your Message" rows={9} resize="none" value={message} onChange={(e) => setMessage(e.target.value)} />
+                                </FormControl>
+                                <Button
+                                    type="submit"
+                                    color='black'
+                                    variant='outline'
+                                >
+                                    Send Message
+                                </Button>
+                            </VStack>
+                        </Box>
+                    </FadeInWhenVisible>
                 </form>
             </div>
         </section>
