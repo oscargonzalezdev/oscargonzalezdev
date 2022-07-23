0fd81cdaf373a08829c2ce34c7e844765bcf6c4c
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-07-23 11:33:48 +0200

    feat(layout): add responsive navbar, fix full width for mobile view

--- components/Layout.js
+++ components/Layout.js
@@ -1,31 +1,40 @@
 import Link from "next/link";
 import Footer from "./Footer"
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
 
 export function NavBar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <header className="header">
-      <span id="logo"><Link href='/'>{'< OG />'}</Link></span>
-      <nav>
-        <ul className="flex-row">
-        <li>
-            <Link href='/blog'>Blog</Link>
-          </li>
-          <li>
-            <Link href='/#works'>Works</Link>
-          </li>
-          <li>
-            <Link href='/#contact'>Contact</Link>
-          </li>
+<header>
+      <nav className="site-navbar">
+      <Link href='/'>{'<OG/>'}</Link>
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li onClick={closeMobileMenu}><Link href='/blog'>Blog</Link></li>
+          <li onClick={closeMobileMenu}><Link href='/#works'>Works</Link></li>
+          <li onClick={closeMobileMenu}><Link href='/#contact'>Contact</Link></li>
         </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseIcon className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
       </nav>
-    </header>
+</header>
   )
 }
 function Layout(props) {
   return (
-    <div className="main-container">
+    <div>
       <NavBar />
+      <div className="main-container">
       <main>{props.children}</main>
+      </div>
       <Footer />
     </div>
   )
