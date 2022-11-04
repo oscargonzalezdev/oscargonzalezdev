0630414908beb51f7b87063cdd9ea786ba32a0da
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-11-04 19:02:28 +0000

    refactor(Layout): create component directory and relocate refactored component

---
+++ components/Layout/Layout.js
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Footer from "../Footer"
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
+import { useState } from "react";
+import styles from './Layout.module.css'
+
+export function NavBar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <header className={styles.header}>
+      <Link href='/'>{'<OG/>'}</Link>
+      <nav className="site-navbar">
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li onClick={closeMobileMenu}><Link href='/blog'>Blog</Link></li>
+          <li onClick={closeMobileMenu}><Link href='/#works'>Works</Link></li>
+          <li className="link-contact" onClick={closeMobileMenu}><Link href='/#contact'>Contact</Link></li>
+        </ul>
+        <div className="mobile-menu" onClick={handleClick}>
+          {click ? (
+            <CloseIcon className="menu-icon" />
+          ) : (
+            <MenuIcon className="menu-icon" />
+          )}
+        </div>
+      </nav>
+    </header>
+  )
+}
+function Layout(props) {
+  return (
+    <div className="layout">
+      <NavBar />
+      <div className="main-container">
+        <main>{props.children}</main>
+      </div>
+      {/* <Footer /> */}
+    </div>
+  )
+}
+export default Layout;
