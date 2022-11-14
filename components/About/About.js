0ddd3a661fb3c92b8ba3bde844527c6619c83f7f
Oscar González <oscargonzalezpena@gmail.com>
2022-11-14 11:22:52 +0000

    refactor(about): update component classes and css rules, move animations to own component

--- components/About/About.js
+++ components/About/About.js
@@ -1,6 +1,5 @@
 import styles from './About.module.css'
 import { Image, Box } from "@chakra-ui/react"
-import { motion } from "framer-motion";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -8,6 +7,7 @@ import Link from "next/link";
 import ParticlesBackground from "../Particles/ParticlesBackground";
 import { useContext } from 'react';
 import { DataContext } from '../../context/DataContext';
+import FadeIn from '../Aminations/FadeIn';
 
 function About() {
   const { elementInView } = useContext(DataContext)
@@ -15,30 +15,22 @@ function About() {
   return (
     <section id="about">
       <ParticlesBackground />
-      <motion.div
-        initial={{ opacity: 0, x: "10%" }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ ease: "easeOut", duration: 1 }}
-      >
+      <FadeIn>
         <Box className='flex-row'>
-          <Box className={styles.hexagonBackground}>
-            <Box className={styles.hexagonBorder}>
-              <Image
-                className={styles.avatar}
-                src='../images/oscargonzalez.jpg'
-                alt='Oscar Gonzalez'
-              />
-            </Box>
-          </Box>
+          <Image
+            className={styles.avatar}
+            src='../images/oscargonzalez.jpg'
+            alt='Oscar Gonzalez'
+          />
           <Box className='flex-column' gap='15px'>
             <Box className={styles.aboutHeader}>
               <h1>Oscar González</h1>
               <h2>Web Developer</h2>
             </Box>
             <span className='social-links'>
-              <a href="https://github.com/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><GitHubIcon sx={{ fontSize: 25 }} /></a>
-              <a href="https://www.linkedin.com/in/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><LinkedInIcon sx={{ fontSize: 25 }} /></a>
-              <a href="https://twitter.com/oscardev_" target="_blank" rel="noopener noreferrer" ><TwitterIcon sx={{ fontSize: 25 }} /></a>
+              <a href="https://github.com/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><GitHubIcon/></a>
+              <a href="https://www.linkedin.com/in/oscargonzalezdev" target="_blank" rel="noopener noreferrer" ><LinkedInIcon/></a>
+              <a href="https://twitter.com/oscardev_" target="_blank" rel="noopener noreferrer" ><TwitterIcon/></a>
             </span>
           </Box>
         </Box>
@@ -46,7 +38,7 @@ function About() {
           Passionate about building digital solutions and learning new technologies, my mission is to help teams develop high-quality web applications that can benefit companies, clients and our planet.
         </p>
         <ul className={styles.links}>
-          <li style={elementInView.works & !elementInView.posts & !elementInView.contact ? { color: 'var(--color-primary)' } : null}>
+          <li className= {elementInView.works & !elementInView.posts & !elementInView.contact ? styles.selectedLink : null}>
             <hr className={elementInView.works & !elementInView.posts & !elementInView.contact ? styles.inView : styles.notInView} />
             <Link
               href='/#works'
@@ -54,16 +46,16 @@ function About() {
               Works
             </Link>
           </li>
-          <li style={!elementInView.works & elementInView.posts & !elementInView.contact ? { color: 'var(--color-primary)' } : null}>
+          <li className= {!elementInView.works & elementInView.posts & !elementInView.contact ? styles.selectedLink : null}>
             <hr className={!elementInView.works & elementInView.posts & !elementInView.contact ? styles.inView : styles.notInView} />
             <Link href='/#blog'>Blog</Link>
           </li>
-          <li style={!elementInView.works & !elementInView.posts & elementInView.contact ? { color: 'var(--color-primary)' } : null}>
+          <li className= {!elementInView.works & !elementInView.posts & elementInView.contact ? styles.selectedLink : null}>
             <hr className={!elementInView.works & !elementInView.posts & elementInView.contact ? styles.inView : styles.notInView} />
             <Link href='/#contact'>Contact</Link>
           </li>
         </ul>
-      </motion.div>
+      </FadeIn>
     </section>
   )
 }
